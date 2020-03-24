810443203c15bba8e8a4b7eb25f019130f5505a2
FernandoAldair <44904273+FernandoAldair@users.noreply.github.com>
2020-03-24 16:52:59 +0100

    help

--- src/com/company/ventana.java
+++ src/com/company/ventana.java
@@ -1,4 +1,16 @@
 package com.company;
 
+import javax.swing.*;
+
 public class ventana {
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
+    private JButton button9;
+    private JTextPane textPane1;
 }
